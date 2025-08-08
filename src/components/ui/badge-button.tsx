84a5b7d52ce011f6e9a13b423a620a927f853f32
01sapankumar <ksapan73@gmail.com>
2025-08-08 23:54:53 +0530

    Update project files

--- src/components/ui/badge-button.tsx
+++ src/components/ui/badge-button.tsx
@@ -1,15 +1,18 @@
-import React from "react"
+import React, { ReactNode } from "react"
 import { IceCreamCone } from "lucide-react"
-
 import { Badge } from "@/components/ui/badge"
 
-const BadgeButton = ({children}) => {
+interface BadgeButtonProps {
+  children: ReactNode
+}
+
+const BadgeButton: React.FC<BadgeButtonProps> = ({ children }) => {
   return (
     <Badge
       variant="outline"
       className="mb-3 cursor-pointer rounded-[14px] border border-black/10 bg-white text-base md:left-6"
     >
-      <IceCreamCone className=" fill-[#9ff01d] stroke-1 text-neutral-800" />{" "}
+      <IceCreamCone className="fill-[#9ff01d] stroke-1 text-neutral-800" />{" "}
       {children}
     </Badge>
   )
